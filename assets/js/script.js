2b10087f35c73fb2270b7fa7b96d66255d3cc2bf
Nicolai Rueckert <nrueckert@Nicolais-MacBook-Pro.local>
2023-09-28 19:12:38 -0500

    Everything works besides local storage and stopping button duplicating

---
+++ assets/js/script.js
@@ -0,0 +1,74 @@
+const searchRequest = $('#searchRequest')
+const searchBtn = $('#searchBtn')
+const prevSearch = $('#prevSearch')
+let latAndLon
+
+function getWeatherApi() {
+    const getLatLon = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchRequest.val() + '&limit=1&appid=2a491b6c9ae1aaa4229bf858b10b21d5'
+    fetch(getLatLon)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function (data) {
+        $('#windSpeed').empty()
+        $('#temps').empty()
+        $('#weather').empty()
+        console.log(data)
+        latAndLon = [
+            lat = data[0].lat,
+            lon = data[0].lon
+        ]
+        if($('#prevSearch').find(`#${searchRequest.val()}`).length = 0){
+        } else {
+        const upperCase = searchRequest.val()
+  		const cityName = upperCase.replace(/\b[a-z]/g, function(cityName) {
+            return cityName.toUpperCase()
+        })
+        $('#prevSearch').append(`<button val='${searchRequest.val}>${cityName}'${searchRequest.val}>${cityName}</button>`)
+    }
+
+
+        console.log(latAndLon[0])
+        const requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + latAndLon[0] + '&lon=' + latAndLon[1] + '&appid=2a491b6c9ae1aaa4229bf858b10b21d5&units=imperial'
+        return fetch(requestUrl)
+    })
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+        const temp = data.list[0].main.temp
+        const feelsLike = data.list[0].main.feels_like
+        const humidity = data.list[0].main.humidity
+        const tempMin = data.list[0].main.temp_min
+        const tempMax = data.list[0].main.temp_max
+        const windSpeed = data.list[0].wind.speed
+        const weather = data.list[0].weather[0].description
+        const upperCase = searchRequest.val()
+  		const cityName = upperCase.replace(/\b[a-z]/g, function(cityName) {
+            return cityName.toUpperCase()
+        })
+        $('#temps').append(`<h1> ${cityName}`)
+        $('#temps').append(`The current temperature is ${temp}°<br>`)
+        $('#temps').append(`The Max Temperature for today is ${tempMax}°<br>`)
+        $('#temps').append(`The Minimum Temperature for today is ${tempMin}°<br>`)
+        $('#temps').append(`It feels like ${feelsLike}°<br>`)
+        $('#temps').append(`The amount of humidity in the air is %${humidity}<rb>`)
+        $('#windSpeed').append(`The windspeed is at ${windSpeed}/mph`)
+        $('#weather').append(`The forecasted weather is ${weather}`)
+        
+
+    })
+    .catch(function(error){
+        console.log('Error fetching data', error)
+    })
+    
+}
+searchBtn.click(function(){
+    getWeatherApi()
+})
+prevSearch.on('click', 'button', function(){
+    searchRequest.val(this.textContent)
+    getWeatherApi()
+    return searchRequest
+})
